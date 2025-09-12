3192d6a50ac67a9b14e42e5ae1af5582530793e8
米娅·罗斯利亚科娃  🇨🇳 <itts.chinitaaa@gmail.com>
2025-09-12 01:18:23 -0300

    Update _fake.js

--- plugins/_fake.js
+++ plugins/_fake.js
@@ -1,11 +1,11 @@
 export async function before(m, { conn }) {
 
-var canal = 'https://whatsapp.com/channel/0029VbApwZ9ISTkEBb6ttS3F'
-var canal2 = 'https://whatsapp.com/channel/0029Vb6IdnEGU3BTahqaLL2V'
-var canal3 = 'https://whatsapp.com/channel/0029VbBVnhSBlHpUWKo19m2m'
+var canal = 'https://whatsapp.com/channel/0029Vb73g1r1NCrTbefbFQ2T'
+var canal2 = 'https://whatsapp.com/channel/0029Vb6GYInD8SDuyzHk3f3l'
+var canal3 = 'https://whatsapp.com/channel/0029Vb6KW2Y0AgW5RA4QIS1B'
 var api = 'https://api.stellarwa.xyz'
-var git = 'https://github.com/DevAlexJs'
-var md = 'https://github.com/DevAlexJs/SakuraBot-MD'
+var git = 'https://github.com/ittschinitaaa'
+var md = 'https://github.com/ittschinitaaa/china-bot'
 
 globalThis.redes = [canal, canal2, canal3, api, git, md].getRandom()
 

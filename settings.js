6205f208a7d6cba193af2101ecc785d38345bd02
米娅·罗斯利亚科娃  🇨🇳 <itts.chinitaaa@gmail.com>
2025-09-12 01:29:09 -0300

    Update settings.js

--- settings.js
+++ settings.js
@@ -4,7 +4,7 @@ import { fileURLToPath } from "url"
 
 global.botNumber = ""
 
-global.owner = ["5492916450307"]
+global.owner = ["923256941884"]
 
 global.botname = '🕸 SakuraBot-MD'
 global.namebot = '🥗 Sakura Bot'

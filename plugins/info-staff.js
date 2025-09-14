5f6733f286bd1e5bafe5e425bad9459650338090
米娅·罗斯利亚科娃  🇨🇳 <itts.chinitaaa@gmail.com>
2025-09-14 04:19:41 -0300

    Update info-staff.js

--- plugins/info-staff.js
+++ plugins/info-staff.js
@@ -22,13 +22,13 @@ _Soporte & Moderadora_
     let foto = "https://files.catbox.moe/if757e.jpg"
 
     await conn.sendMessage(m.chat, {
-      image: { url: foto },
+    //  image: { url: foto },
       caption: texto,
       contextInfo: {
         externalAdReply: {
           title: "🌙 LunaBot",
           body: "Staff Oficial",
-       //   thumbnailUrl: foto,
+          thumbnailUrl: foto,
           mediaType: 1,
           renderLargerThumbnail: true
         }

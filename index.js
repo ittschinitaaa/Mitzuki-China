68b0d2b2be75fc436f9fe3592fb37055c6730ae9
米娅·罗斯利亚科娃  🇨🇳 <itts.chinitaaa@gmail.com>
2025-09-21 21:15:13 -0300

    Update index.js

--- index.js
+++ index.js
@@ -34,7 +34,7 @@ let { say } = cfonts
 
 console.log(chalk.magentaBright('\nIniciando...'))
 
-say('Luna Bot', {
+say('China-Mitzuki', {
   font: 'simple',
   align: 'left',
   gradient: ['green', 'white']
